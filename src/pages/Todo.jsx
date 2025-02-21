2ab9211e94cda9c23cd8edb8f4b18386379721cb
JBR4N <jmanulat9@gmail.com>
2025-02-21 16:51:26 +0800

    3rd commit

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -66,7 +66,7 @@ const Todo = () => {
           </CardContent>
 
           <CardFooter className="flex justify-between">
-            <h1>Made with ♥ by Mario Inguito</h1>
+            <h1>Made with ♥ by JBR4N</h1>
           </CardFooter>
         </Card>
       </div>
