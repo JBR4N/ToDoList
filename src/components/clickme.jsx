72e3a65ccd084a63cdc1381f514c4207aac2df58
JBR4N <jmanulat9@gmail.com>
2025-02-21 16:07:19 +0800

    1st commit

---
+++ src/components/clickme.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const Clickme = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default Clickme;
